ff6b2954d3553f5fc802dae644deaf93ec205e57
andrew <andrewjohnlay@gmail.com>
2022-09-02 19:41:30 +1000

    Linux Sh file

---
+++ Emscripten/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+em++ -O0 -g \
+-o EmscriptenProcessGameMessages.js \
+EmscriptenCanvasWorker.cpp \
+EmscriptenProcessGameMessages.cpp \
+../JSWasm/CanvasWorker.cpp \
+-pthread -v -s MODULARIZE=1  \
+--no-entry -s "EXPORTED_RUNTIME_METHODS='getValue', 'setValue', 'cwrap', 'ccall', 'printErr', 'EmVal'" \
+-s PTHREAD_POOL_SIZE=3  -s "EXPORT_NAME='ScummWebModule'" -s"LLD_REPORT_UNDEFINED" -s"TOTAL_MEMORY=1024MB" \
+-s OFFSCREENCANVAS_SUPPORT=1 --bind
